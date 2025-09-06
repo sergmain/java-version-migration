4f6c44a3b103a0acf11f54aba364afd2eee95bd9
sergmain <serg_main@yahoo.com>
2025-09-06 13:37:48 -0700

    added Angular migration - MathUnitAngularMigration

---
+++ src/main/java/metaheuristic/java_version_migration/migrations/MathUnitAngularMigration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2025. Sergio Lissner
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package metaheuristic.java_version_migration.migrations;
+
+import metaheuristic.java_version_migration.Migration;
+import metaheuristic.java_version_migration.data.Content;
+
+/**
+ * @author Sergio Lissner
+ * Date: 9/6/2025
+ * Time: 1:05 PM
+ */
+public class MathUnitAngularMigration {
+
+    public static Content process(Migration.MigrationConfig cfg, String content) {
+        String newContent = migrateMathUnitAngular(content);
+        Content result = new Content(newContent, !newContent.equals(content));
+        return result;
+    }
+
+    public static String migrateMathUnitAngular(String content) {
+        String result = content;
+        
+        // Replace unit( with math.unit( only when not already prefixed with math.
+        result = result.replaceAll("(?<!math\\.)\\bunit\\s*\\(", "math.unit(");
+        
+        // Add 'use sass:math'; at the top if not already present and unit functions were found
+        if (!content.equals(result) && !result.contains("use 'sass:math'")) {
+            result = "use 'sass:math';\n" + result;
+        }
+        
+        return result;
+    }
+}
---
+++ src/test/java/metaheuristic/java_version_migration/migrations/MathUnitAngularMigrationTest.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (c) 2025. Sergio Lissner
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package metaheuristic.java_version_migration.migrations;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
+
+/**
+ * @author Sergio Lissner
+ * Date: 9/6/2025
+ * Time: 1:11 PM
+ */
+@Execution(CONCURRENT)
+public class MathUnitAngularMigrationTest {
+
+    @Test
+    void testMigrateSingleUnitCall() {
+        String input = """
+                .element {
+                  top: unit(6);
+                }
+                """;
+        
+        String expected = """
+                use 'sass:math';
+                .element {
+                  top: math.unit(6);
+                }
+                """;
+        
+        String result = MathUnitAngularMigration.migrateMathUnitAngular(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testMigrateMultipleUnitCalls() {
+        String input = """
+                .element {
+                  top: unit(6);
+                  left: unit(10);
+                  margin: unit(5) unit(3);
+                }
+                """;
+        
+        String expected = """
+                use 'sass:math';
+                .element {
+                  top: math.unit(6);
+                  left: math.unit(10);
+                  margin: math.unit(5) math.unit(3);
+                }
+                """;
+        
+        String result = MathUnitAngularMigration.migrateMathUnitAngular(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testMigrateUnitWithSpaces() {
+        String input = """
+                .element {
+                  top: unit (6);
+                  left: unit  (10);
+                }
+                """;
+        
+        String expected = """
+                use 'sass:math';
+                .element {
+                  top: math.unit(6);
+                  left: math.unit(10);
+                }
+                """;
+        
+        String result = MathUnitAngularMigration.migrateMathUnitAngular(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testNoChangeWhenAlreadyHasSassMath() {
+        String input = """
+                use 'sass:math';
+                .element {
+                  top: unit(6);
+                }
+                """;
+        
+        String expected = """
+                use 'sass:math';
+                .element {
+                  top: math.unit(6);
+                }
+                """;
+        
+        String result = MathUnitAngularMigration.migrateMathUnitAngular(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testNoChangeWhenNoUnitCalls() {
+        String input = """
+                .element {
+                  top: 10px;
+                  left: 20rem;
+                  margin: 5em;
+                }
+                """;
+        
+        String result = MathUnitAngularMigration.migrateMathUnitAngular(input);
+        assertEquals(input, result);
+    }
+
+    @Test
+    void testNoChangeWhenAlreadyMathUnit() {
+        String input = """
+                use 'sass:math';
+                .element {
+                  top: math.unit(6);
+                  left: math.unit(10);
+                }
+                """;
+        
+        String result = MathUnitAngularMigration.migrateMathUnitAngular(input);
+        assertEquals(input, result);
+    }
+
+    @Test
+    void testEmptyContent() {
+        String input = "";
+        String result = MathUnitAngularMigration.migrateMathUnitAngular(input);
+        assertEquals(input, result);
+    }
+
+    @Test
+    void testComplexSassWithMixins() {
+        String input = """
+                @mixin button-style {
+                  padding: unit(8) unit(16);
+                  border-radius: unit(4);
+                }
+                
+                .button {
+                  @include button-style;
+                  margin: unit(10);
+                }
+                """;
+        
+        String expected = """
+                use 'sass:math';
+                @mixin button-style {
+                  padding: math.unit(8) math.unit(16);
+                  border-radius: math.unit(4);
+                }
+                
+                .button {
+                  @include button-style;
+                  margin: math.unit(10);
+                }
+                """;
+        
+        String result = MathUnitAngularMigration.migrateMathUnitAngular(input);
+        assertEquals(expected, result);
+    }
+}
